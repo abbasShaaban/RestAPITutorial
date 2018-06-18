8ba778bac9864579745ad62ff5ee337de0d7cf7d
abbasShaaban <aas100@mail.aub.edu>
2018-06-18 12:56:00 +0300

    DELETE todos/:id and PATCH todos/:id

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser');
 var mongoose = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {ObjectID} = require('mongodb');
 
 var app = express();
 
@@ -23,4 +24,56 @@ app.post('/todos',(req,res)=>{
         res.send(err.errors.text.message);
     });
 });
+
+app.get('/todos',  (req, res)=>{
+
+    Todo.find().then((todos)=>{
+        res.send(todos);
+    },(err)=>{
+        res.status(200).send(err);
+    });
+    console.log(res);
+});
+app.get('/todos',  (req, res)=>{
+
+    Todo.find().then((todos)=>{
+        res.send(todos);
+    },(err)=>{
+        res.status(200).send(err);
+    });
+    console.log(res);
+});
+app.get('/todos/:id',(req, res)=>{
+    let id = req.params.id;
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+    Todo.findById(id).then((todos)=>{
+        if(!todos){
+            res.status(404).send({});
+        }
+        res.send(todos);
+    },(err)=>{
+        res.status(400).send({});
+    });
+    console.log(res);
+});
+
+app.delete('/todos/:id',(req,res)=>{
+    let id = req.params.id;
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+    Todo.findByIdAndRemove(id).then((todo)=>{
+        console.log('abbas');
+        if(!todo)
+           return res.status(404).send();
+        res.status(200).send(todo);
+    },(err)=>{
+        res.status(400).send();
+    });
+});
+
+
+
 
